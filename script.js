667eebf0071d308888afcc831da0e6d78eccc30c
nicol29 <nicolascondrea@gmail.com>
2022-11-04 17:36:55 +0000

    Add eventListener for button input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice(e) {
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice;
     
@@ -10,25 +10,27 @@ function getComputerChoice() {
         computerChoice = "scissors"
     }
 
-    return computerChoice;
+    //return computerChoice;
+    console.log(e);
 }
 
 
-function playerSelection() {
-    let playerChoice = prompt("Choose rock paper or scissors");
+function playerSelection(selection) {
+    let playerChoice = selection;
     
     return playerChoice;
 }
 
 
-function gameLogic() {
+function gameLogic(playerChoice) {
     let playerScore = 0;
     let computerScore = 0;
     let result = false;
 
     while(!result){
         let computerChoice = getComputerChoice();
-        let playerChoice = playerSelection();
+        let playerChoice = playerChoice();
+        console.log(playerChoice);
 
         if(computerChoice == "rock" && playerChoice == "scissors"){
             alert("You lost, computer chose " + computerChoice);
@@ -61,4 +63,11 @@ function gameLogic() {
 }
 
 //gameLogic();
+
+const availableGamePicks = document.querySelectorAll(".selection");
+
+
+availableGamePicks.forEach(Pick => Pick.addEventListener(("click"), (event) =>{
+    console.log(getComputerChoice(event));
+}));
 
